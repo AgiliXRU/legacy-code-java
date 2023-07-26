f22e56c4720259c2cf6a4e8980b538cf74124d93
AndreyTW <sav_687@mail.ru>
2023-07-26 10:21:19 +0300

    DeliveryController unit tests were added

---
+++ src/test/java/ru/agilix/workshop/legacy/DeliveryControllerShould.java
@@ -0,0 +1,48 @@
+package ru.agilix.workshop.legacy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeliveryControllerShould {
+
+    /*    Примеры:
+                | Тип клиента | Сумма товаров в корзине | Сумма заказа |
+                | "Обычный"   | 999                     | 1249         |
+                | "Обычный"   | 1000                    | 1000         |
+                | "VIP"       | 999                     | 999          |
+                | "VIP"       | 2499                    | 2499         |
+                | "VIP"       | 2500                    | 2375         |*/
+    DeliveryController deliveryController;
+
+    @BeforeEach
+    void setUp() {
+        deliveryController = new DeliveryController();
+    }
+
+    @Test
+    void calculate_sum_of_order_1249_when_Obichny_type_and_sum_of_basket_999_are_given() {
+        assertEquals(1249, deliveryController.calculate("Обычный", 999).intValue());
+    }
+
+    @Test
+    void calculate_sum_of_order_1000_when_Obichny_type_and_sum_of_basket_1000_are_given() {
+        assertEquals(1000, deliveryController.calculate("Обычный", 1000).intValue());
+    }
+
+    @Test
+    void calculate_sum_of_order_999_when_VIP_type_and_sum_of_basket_999_are_given() {
+        assertEquals(999, deliveryController.calculate("VIP", 999).intValue());
+    }
+
+    @Test
+    void calculate_sum_of_order_2499_when_VIP_type_and_sum_of_basket_2499_are_given() {
+        assertEquals(2499, deliveryController.calculate("VIP", 2499).intValue());
+    }
+
+    @Test
+    void calculate_sum_of_order_2500_when_VIP_type_and_sum_of_basket_2375_are_given() {
+        assertEquals(2375, deliveryController.calculate("VIP", 2500).intValue());
+    }
+}
